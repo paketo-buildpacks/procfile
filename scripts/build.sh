93bf6d515ca8da7990b926f9d1dbbe5371a595ce
Ben Hale <bhale@pivotal.io>
2020-03-04 13:49:20 -0800

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,5 @@ if [[ -d ../go-cache ]]; then
   export GOPATH
 fi
 
-GOOS="linux" go build -ldflags='-s -w' -o bin/build cmd/build/main.go
-GOOS="linux" go build -ldflags='-s -w' -o bin/detect cmd/detect/main.go
+GOOS="linux" go build -ldflags='-s -w' -o bin/build cmd/build
+GOOS="linux" go build -ldflags='-s -w' -o bin/detect cmd/detect
