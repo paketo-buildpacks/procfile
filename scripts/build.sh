76fc0d86f31dc2f02749c8e68827687d4e55fb19
Anthony Dahanne <anthony.dahanne@gmail.com>
2024-03-15 16:22:24 -0400

    Remove pack exp for now * it'll come via official pipeline builder release

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,18 +2,18 @@
 
 set -euo pipefail
 
-GOOS="linux" go build -ldflags='-s -w' -o linux/amd64/bin/main github.com/paketo-buildpacks/procfile/v5/cmd/main
-GOOS="linux" GOARCH="arm64" go build -ldflags='-s -w' -o linux/arm64/bin/main github.com/paketo-buildpacks/procfile/v5/cmd/main
+GOOS="linux" go build -ldflags='-s -w' -o bin/main github.com/paketo-buildpacks/procfile/v5/cmd/main
+GOOS="windows" GOARCH="amd64" go build -ldflags='-s -w' -o bin/main.exe github.com/paketo-buildpacks/procfile/v5/cmd/main
 
 if [ "${STRIP:-false}" != "false" ]; then
-  strip linux/amd64/bin/main linux/arm64/bin/main
+  strip bin/main bin/main.exe
 fi
 
 if [ "${COMPRESS:-none}" != "none" ]; then
-  $COMPRESS linux/amd64/bin/main linux/arm64/bin/main
+  $COMPRESS bin/main bin/main.exe
 fi
 
-ln -fs main linux/amd64/bin/build
-ln -fs main linux/arm64/bin/build
-ln -fs main linux/amd64/bin/detect
-ln -fs main linux/arm64/bin/detect
+ln -fs main bin/build
+ln -fs main bin/detect
+ln -fs main.exe bin/build.exe
+ln -fs main.exe bin/detect.exe
